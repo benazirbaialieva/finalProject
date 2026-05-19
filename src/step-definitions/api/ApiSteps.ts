c617a1545947855b2bbe3af4ef5a9e5baf4bd9f3
Benazir Baialieva <benazir.baialieva@gmail.com>
2026-05-19 11:29:34 -0700

    added initial configs

---
+++ src/step-definitions/api/ApiSteps.ts
@@ -0,0 +1,13 @@
+import { Given, When, Then } from '@cucumber/cucumber'
+
+Given('doctor is logged in', function () {
+
+});
+
+When('user hits GET {string}', function (endpoint) {
+
+});
+
+Then('verify status code is {int}', function (expectedStatusCode) {
+
+});
